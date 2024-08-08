59297733dfaebe191f96ee267a73450edd7ddc31
EC2 Default User <ec2-user@ip-172-31-10-204.ap-south-1.compute.internal>
2024-08-08 06:45:44 +0000

    myfrist commit

---
+++ bankcode.java
@@ -0,0 +1,20 @@
+package com.vansro.payments;
+
+public class App
+
+{
+
+public static void main(String[] args)
+
+{
+
+System.out.println( "Welcome to Vansro");
+
+System.out.println( "Student Login Page");
+
+System.out.println( "Enter User Id:" );
+
+System.out.println( "Enter Password:" );
+}
+}
+
